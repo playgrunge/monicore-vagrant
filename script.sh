120b454026924b0ca102fad7479db74384745e35
bodyjar <bodyjar@hotmail.com>
2014-11-27 13:30:18 -0500

    go 1.1

--- script.sh
+++ script.sh
@@ -4,6 +4,6 @@ sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -q -y software-properties-common python-software-properties
 sudo add-apt-repository ppa:git-core/ppa
-sudo add-apt-repository ppa:gophers/go
+sudo add-apt-repository ppa:duh/golang
 sudo apt-get update -q
 sudo apt-get install -q -y git golang-stable
