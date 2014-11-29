76fbc537b712f9c80234b380201f1329eb2fa75e
bodyjar <bodyjar@hotmail.com>
2014-11-29 16:00:23 -0500

    fix silent command

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ sudo apt-get update -qq
 sudo apt-get install -q -y software-properties-common python-software-properties
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo add-apt-repository -y ppa:duh/golang
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo apt-get update -qq
 sudo apt-get install -q -y git golang 
 sudo apt-get install -q -y libgtk2.0-0 sublime-text-installer
