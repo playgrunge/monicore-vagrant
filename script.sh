e750c3c8ecb6f30b0838412ba284ed3792a288c2
Louis Cloutier <louisphp@gmail.com>
2014-11-28 23:51:27 -0500

    Fix the error when trying to install golang.

--- script.sh
+++ script.sh
@@ -2,8 +2,9 @@
 sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
 
 export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update -qq
 sudo apt-get install -q -y software-properties-common python-software-properties
-sudo add-apt-repository ppa:git-core/ppa
-sudo add-apt-repository ppa:duh/golang
-sudo apt-get update -q
-sudo apt-get install -q -y git golang-stable
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo add-apt-repository -y ppa:duh/golang
+sudo apt-get update -qq
+sudo apt-get install -q -y git golang
