2af08e33936799b4d9d41b6baa0de6f03a9f4d94
bodyjar <bodyjar@hotmail.com>
2014-11-29 16:28:26 -0500

    remove error on creating existing folder

--- setup-project.sh
+++ setup-project.sh
@@ -3,7 +3,7 @@ echo "export GOPATH=/home/vagrant/gocode" >> ~/.profile
 echo "export PATH=$PATH:/home/vagrant/gocode/bin" >> ~/.profile
 . ~/.profile
 
-mkdir ~/gocode
+mkdir -p ~/gocode
 go get "github.com/gorilla/mux"
 go get "github.com/codegangsta/gin"
 mkdir -p $GOPATH/src/github.com/playgrunge
