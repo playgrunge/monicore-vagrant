70a681eebce274e69f420e8d660c1ae502825580
Louis Cloutier <louisphp@gmail.com>
2014-11-29 15:06:05 -0500

    Add Package Control to sublime-text-3

--- setup-project.sh
+++ setup-project.sh
@@ -8,3 +8,7 @@ go get "github.com/gorilla/mux"
 go get "github.com/codegangsta/gin"
 mkdir -p $GOPATH/src/github.com/playgrunge
 git clone https://github.com/playgrunge/monicore.git $GOPATH/src/github.com/playgrunge/monicore
+
+mkdir -p ~/'.config/sublime-text-3/Installed Packages/'
+wget "https://sublime.wbond.net/Package%20Control.sublime-package"
+mv 'Package Control.sublime-package' ~/'.config/sublime-text-3/Installed Packages/'
