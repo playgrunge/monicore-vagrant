0f8ef75f3930a9ba4d516fcb8b9088f8925ec58e
bodyjar <bodyjar@hotmail.com>
2015-01-11 14:54:59 -0500

    installation of mongodb

--- script.sh
+++ script.sh
@@ -17,3 +17,8 @@ sudo apt-get install -y nodejs
 
 sudo npm install -g bower
 sudo npm install -g grunt-cli
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update -qq
+sudo apt-get install -q -y mongodb-org
