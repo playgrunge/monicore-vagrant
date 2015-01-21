16ae45b698d7578cb9e982f6dbc533d308fe172f
bodyjar <bodyjar@hotmail.com>
2015-01-21 11:50:04 -0500

    add mercurial for some go packages

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo apt-get update -qq
 sudo apt-get install -q -y libgtk2.0-0 sublime-text-installer
 
-sudo apt-get install -q -y git curl
+sudo apt-get install -q -y git curl mercurial
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get install -y nodejs
 
